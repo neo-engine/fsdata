1b0a3f95fecb295760fdaeb574dc5901630f8e1d
PHP Wellnitz <philipwellnitz@gmx.de>
2022-08-07 05:41:56 +0900

    inf cave

--- data/scripts/147.m4.h
+++ data/scripts/147.m4.h
@@ -4,7 +4,7 @@
 // if on high tide, warp to hid tide map
 GET_CURRENT_HOURS_MOD_TWELVE
 
-CHECK_REGISTER_L( 0, 6, 6 )
+CHECK_REGISTER_L( 0, 6, 7 )
 
 // high tide position
 SET_REGISTER( 0, 1 ) // type (cave entry)
@@ -13,6 +13,7 @@ SET_REGISTER( 2, 1 ) // mapy
 SET_REGISTER( 3, 3 ) // mapx
 SET_REGISTER( 4, 30 ) // y
 SET_REGISTER( 5, 20 ) // x
+SET_REGISTER( 6, 3 ) // z
 
 
 
@@ -23,6 +24,7 @@ SET_REGISTER( 2, 1 ) // mapy
 SET_REGISTER( 3, 1 ) // mapx
 SET_REGISTER( 4, 30 ) // y
 SET_REGISTER( 5, 20 ) // x
+SET_REGISTER( 6, 3 ) // z
 
 
 
---
+++ data/scripts/152.m4.h
@@ -0,0 +1,11 @@
+// infinity cave warp script reserved/entry5
+#include "scriptlib.m4.h"
+
+SET_REGISTER( 0, 1 ) // type (cave entry)
+SET_REGISTER( 1, 99 ) // bank
+SET_REGISTER( 2, 1 ) // mapy
+SET_REGISTER( 3, 1 ) // mapx
+SET_REGISTER( 4, 27 ) // y
+SET_REGISTER( 5, 15 ) // x
+SET_REGISTER( 6, 3 ) // z
+
