33bb0a12a5405c5cb3be9ae6fa53d2035dd4b3d0
PHP Wellnitz <philipwellnitz@gmx.de>
2022-08-07 05:55:03 +0900

    shoal cave (2)

--- data/scripts/147.m4.h
+++ data/scripts/147.m4.h
@@ -4,7 +4,7 @@
 // if on high tide, warp to hid tide map
 GET_CURRENT_HOURS_MOD_TWELVE
 
-CHECK_REGISTER_L( 0, 6, 7 )
+CHECK_REGISTER_L( 0, 6, 8 )
 
 // high tide position
 SET_REGISTER( 0, 1 ) // type (cave entry)
@@ -14,7 +14,7 @@ SET_REGISTER( 3, 3 ) // mapx
 SET_REGISTER( 4, 30 ) // y
 SET_REGISTER( 5, 20 ) // x
 SET_REGISTER( 6, 3 ) // z
-
+SKIP( 7 )
 
 
 // default position
